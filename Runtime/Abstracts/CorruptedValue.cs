4ca42c997e1b16f9f82740002ffb66a5636b92eb
SEANTHEBOMB <sean.feeser@gmail.com>
2024-08-10 17:26:00 -0400

    CorruptedValue has OnChangedEvent

--- Runtime/Abstracts/CorruptedValue.cs
+++ Runtime/Abstracts/CorruptedValue.cs
@@ -10,9 +10,35 @@ namespace Corrupted
     /// <typeparam name="T"></typeparam>
     public abstract class CorruptedValue<T> : CorruptedValue
     {
-        public T Value;
 
         public override object ObjectValue => Value;
+
+        [FormerlySerializedAs("Value")]
+        public T _value;
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                OnValueChanged?.Invoke(_value, value);
+                _value = value;
+            }
+        }
+
+        /// <summary>
+        /// T1 = old, T2 = new
+        /// </summary>
+        public System.Action<T, T> OnValueChanged;
+
+
+        public void SetValue(T value)
+        {
+            Value = value;
+        }
     }
 
     public abstract class CorruptedValue : CorruptedModel
